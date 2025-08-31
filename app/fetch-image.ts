c1a16107e3f26adf8a94ab8828f80bb82e9afedb
genkichi <genshigoto0821@gmail.com>
2025-08-31 22:30:25 +0900

    update: APIKEY削除

--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -1,6 +1,4 @@
 'use server';
-
-import { CAT_API_KEY } from './env';
 // 画像情報の型定義
 type Image = {
   url: string;
@@ -8,9 +6,7 @@ type Image = {
 
 // APIから画像を取得する関数
 export async function fetchImage(): Promise<Image> {
-  const res = await fetch('https://api.thecatapi.com/v1/images/search', {
-    headers: { 'x-api-key': CAT_API_KEY },
-  });
+  const res = await fetch('https://api.thecatapi.com/v1/images/search');
   const images: unknown = await res.json();
   console.log('fetchImage: 画像情報を取得しました', images);
   if (!isImageArray(images)) {
